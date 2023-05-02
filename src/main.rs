7743df952b25725d07c032705f9f6af1069ede97
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-05-02 19:09:32 +0300

    Update remove_dir_all, clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use std::fs;
-use std::io::Result as IoResult;
-use std::path::Path;
-
-use remove_dir_all::*;
+use remove_dir_all::ensure_empty_dir;
 
 fn get_path_or_print_help() -> Option<String> {
     let args = std::env::args();
@@ -14,14 +10,8 @@ fn get_path_or_print_help() -> Option<String> {
     }
 }
 
-fn main() -> IoResult<()> {
-    let raw_path = get_path_or_print_help().unwrap();
-    let tmp_path = Path::new(&raw_path);
-    if tmp_path.exists() {
-        println!("removing...");
-        remove_dir_all(tmp_path)?;
-    }
-    println!("creating...");
-    fs::create_dir(tmp_path)?;
+fn main() -> std::io::Result<()> {
+    let path = get_path_or_print_help().unwrap();
+    ensure_empty_dir(path)?;
     Ok(())
 }
